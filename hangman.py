fc4de76c1a6edc934dee5128648b349e050de668
Vincent Le <vincent.le12@yahoo,com>
2021-07-23 12:51:19 -0700

    hangman stuff

---
+++ hangman.py
@@ -0,0 +1,124 @@
+import time
+import random
+
+print("\nWelcome to Hangman game by DataFlair\n")
+name = input("Enter your name: ")
+print("Hello " + name + "! Best of Luck!")
+time.sleep(2)
+print("The game is about to start!\nLet's play Hangman!")
+time.sleep(3)
+
+#One gripe I have about this setup personally is that I would prefer that
+#variables that are intended to be global simply be initialized outside of
+#the function. However, I can see that it would be a bit harder as the variables
+#have to be initialized with some value if done that way.
+def main():
+    global count
+    global display
+    global word
+    global already_guessed
+    global length
+    global play_game
+    words_to_guess = ["january", "border", "image", "film", "promise", "kids",
+                     "lungs", "doll", "rhyme", "damage", "plants"]
+    word = random.choice(words_to_guess)
+    length = len(word)
+    count = 0
+    display = '_' * length
+    already_guessed = []
+    play_game = ""
+    
+def play_loop():
+    global play_game
+    play_game = input("Do you want to play again? y = yes, n = no \n")
+    while play_game not in ["y", "n", "Y", "N"]:
+        play_game = input("Do you want to play again? y = yes, n = no \n")
+    if play_game == "y" or play_game == "Y":
+        main()
+    elif play_game == "n" or play_game == "N":
+        print("Thanks for Playing! We hope to see you again!")
+        exit()
+        
+def hangman():
+    global count
+    global display
+    global word
+    global already_guessed
+    global play_game
+    limit = 5
+    guess = input("This is the Hangman Word: " + display + " Enter your guess: \n")
+    guess = guess.strip()
+    if len(guess.strip()) == 0 or len(guess.strip()) >= 2 or guess <= "9":
+        print("Invalid input, Try a letter\n")
+        hangman()
+    elif guess in word:
+        already_guessed.extend([guess])
+        index = word.find(guess)
+        word = word[:index] + "_" + word[index+1:]
+        display = display[:index] + guess + display[index + 1:]
+        print(display + "\n")
+    
+    elif guess in already_guessed:
+        print("Try another letter.\n")
+        
+    else:
+        count+=1
+        
+        if count == 1:
+            time.sleep(1)
+            print("   _____ \n"
+                  "  |      \n"
+                  "  |      \n"
+                  "  |      \n"
+                  "  |      \n"
+                  "  |      \n"
+                  "  |      \n"
+                  "__|__\n")
+            print("Wrong guess. " + str(limit - count) + " guesses remaining\n")
+            
+        elif count == 2:
+            time.sleep(1)
+            print("   _____ \n"
+                  "  |     | \n"
+                  "  |     |\n"
+                  "  |      \n"
+                  "  |      \n"
+                  "  |      \n"
+                  "  |      \n"
+                  "__|__\n")
+            print("Wrong guess. " + str(limit - count) + " guesses remaining\n")
+            
+        elif count == 4:
+            time.sleep(1)
+            print("   _____ \n"
+                  "  |     | \n"
+                  "  |     |\n"
+                  "  |     | \n"
+                  "  |     O \n"
+                  "  |      \n"
+                  "  |      \n"
+                  "__|__\n")
+            print("Wrong guess. " + str(limit - count) + " last guess remaining\n")
+            
+        elif count == 5:
+            time.sleep(1)
+            print("   _____ \n"
+                  "  |     | \n"
+                  "  |     |\n"
+                  "  |     | \n"
+                  "  |     O \n"
+                  "  |    /|\ \n"
+                  "  |    / \ \n"
+                  "__|__\n")
+            print("Wrong guess. You are hanged!!!\n")
+            print("The word was:", already_guessed ,word)
+            play_loop()
+    if word == "_" * length:
+        print("Congrats! You have guessed the word correctly!")
+        play_loop()
+            
+    elif count != limit:
+            hangman()
+            
+main()
+hangman()
